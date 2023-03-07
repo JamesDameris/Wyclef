74b1efb578ef19c222510ad4508457b0f5dd51b5
jamesdameris <jamesdameris@gmail.com>
2023-03-07 15:53:20 -0800

    Query Finished

--- logicDB.js
+++ logicDB.js
@@ -27,11 +27,6 @@ function parseSentence (sentence) {
     return tokens;
 }
 
-function parseStatement (statement) {
-    // "EQ practice.ticTacToe.T.board.top.l.P1 practice.ticTacToe.T.board.bottom.l.P2"
-    return statement.split(" ");
-}
-
 function insert (sentence) {
     let tokens = parseSentence(sentence);
     let node = DB;
@@ -85,16 +80,114 @@ function unify (sentence) {
     return unified.map(uni=>uni.binding);
 }
 
-function query(statement) {
-    console.log("Querying: " + statement);
-    let pStatement = parseStatement(statement);
-    if (pStatement[0] == "EQ") {
-        return equivQuery(pStatement);
-    } else if (pStatement[0] == "NEQ") {
-        return !equivQuery(pStatement);
-    } else if (pStatement[0] == "NOT") {
-        return notQuery(pStatement);
+function query(statements) {
+    console.log("Querying: " + statements);
+    let possibleBindings = [ {} ];
+
+    for (let i = 0; i < statements.length; i++) {
+        if (statements[i].split(" ")[0] == "EQ" || statements[i].split(" ")[0] == "NEQ" || statements[i].split(" ")[0] == "NOT") {
+            continue;
+        }
+        let iterativeBindings = [];
+        let newBinding = unify(statements[i]);
+        // console.log("New Binding", newBinding);
+        for (let uni of possibleBindings) {
+            for (let binding of newBinding) {
+                let newKeys = Object.keys(binding).filter(k=>!Object.keys(uni).includes(k)); // returns all the keys that are not in uni
+                let oldKeys = Object.keys(binding).filter(k=>Object.keys(uni).includes(k)); //  returns all the keys that are in uni
+                let thereExistsAnIncompatibileKey = oldKeys.some(k=>binding[k]!=uni[k]); // if there isnt at any key in binding that doesn't matches the same key in uni
+                if (thereExistsAnIncompatibileKey) {
+                    continue;
+                } else {
+                    let nextUni = clone(uni); // clone the current compatible bindings 
+                    for (newKey of newKeys) { // and add the new compatible bindings to the set of bindings
+                        nextUni[newKey] = binding[newKey];
+                    }
+                    iterativeBindings.push(nextUni); // then update iterativeBindings (which is reset to empty every unify)
+                }
+            }
+        }
+        console.log("iterartive", iterativeBindings);
+        possibleBindings = iterativeBindings;
     }
+    for (let i = 0; i < statements.length; i++) {
+        if (statements[i].split(" ")[0] == "EQ") {
+            EQStatement = statements[i].split(" ");
+            console.log("Checking Equality on:", EQStatement[1], "and", EQStatement[2]);
+            let iterativeBindings = [];
+            for (let uni of possibleBindings) {
+                if ((EQStatement[1] in uni) && (EQStatement[2] in uni) ) { // If both arguments are bound variables or constants, we compare their values and discard the uni if they're not equal.
+                    if (uni[EQStatement[1]] != uni[EQStatement[2]]) {
+                        continue; // if not equal, discard the uni
+                    } else {
+                        let nextUni = clone(uni); // if equal, keep the uni
+                        iterativeBindings.push(nextUni); 
+                    }
+                } else if ((EQStatement[1] in uni) && !(EQStatement[2] in uni)) { // If one of the two arguments is an unbound variable and one of them is a bound variable or constant, we establish a new binding for the unbound variable so that it's now bound to the same thing as the other argument.
+                    let nextUni = clone(uni);
+                    nextUni[EQStatement[2]] = nextUni[EQStatement[1]];
+                    iterativeBindings.push(nextUni);
+                    
+                } else if (!(EQStatement[1] in uni) && (EQStatement[2] in uni)) {
+                    let nextUni = clone(uni);
+                    nextUni[EQStatement[1]] = nextUni[EQStatement[2]];
+                    iterativeBindings.push(nextUni);
+                
+                } else { // If both arguments are unbound variables, that's an error case – we can print an error message to warn the user that they've done something invalid.
+                    console.log("Error: Neither arguments bound");
+                }
+                possibleBindings = iterativeBindings;
+            }
+        
+        } else if (statements[i].split(" ")[0] == "NEQ") { // NEQ is basically a combination of the NOT and EQ logic, but it doesn't ever add any new bindings (since the user should only specify arguments that are either constants or previously-bound variables to compare – you can't compare the value of an unbound variable to anything, since it doesn't have one).
+            NEQStatement = statements[i].split(" ");
+            console.log("Checking Unequality on:", NEQStatement[1], "and", NEQStatement[2]);
+            let iterativeBindings = [];
+            for (let uni of possibleBindings) {
+                if ((NEQStatement[1] in uni) && (NEQStatement[2] in uni) ) { // If both arguments are bound variables or constants, we compare their values and discard the uni if they're equal.
+                    console.log("Winner:", uni[NEQStatement[1]], "Loser:", uni[NEQStatement[2]]);
+                    if (uni[NEQStatement[1]] == uni[NEQStatement[2]]) {
+                        continue; // if equal, discard the uni
+                    } else {
+                        let nextUni = clone(uni); // if not equal, keep the uni
+                        iterativeBindings.push(nextUni); 
+                    }
+                } else {
+                    // If either argument is unbound variables, that's an error case – we can print an error message to warn the user that they've done something invalid.
+                    console.log("Error:", (NEQStatement[1] in uni) ? NEQStatement[1] + " bound" : NEQStatement[1], (NEQStatement[2] in uni) ? NEQStatement[2] + " bound" : NEQStatement[2], "unbound");
+                    let nextUni = clone(uni); // keeping uni intact since this is a error case and we don't want to break possibleBindings if it fails here for now
+                    iterativeBindings.push(nextUni); 
+                }
+                possibleBindings = iterativeBindings;
+            }
+        } else if (statements[i].split(" ")[0] == "NOT") {
+            // NOT basically drops every uni in possibleBindings for which the operator sentence successfully unifies with the previously bound values (and keeps the rest)
+            let iterativeBindings = [];
+            let newBinding = unify(statements[i].split(" ")[1]);
+            console.log("New Binding", newBinding);
+            for (let uni of possibleBindings) {
+                for (let binding of newBinding) {
+                    let newKeys = Object.keys(binding).filter(k=>!Object.keys(uni).includes(k)); // returns all the keys that are not in uni
+                    let oldKeys = Object.keys(binding).filter(k=>Object.keys(uni).includes(k)); //  returns all the keys that are in uni
+                    let thereExistsOnlyCompatibileKeys = !oldKeys.some(k=>binding[k]!=uni[k]); // if there isnt at any key in binding that doesn't matches the same key in uni
+                    if (thereExistsOnlyCompatibileKeys) {
+                        continue;
+                    } else {
+                        let nextUni = clone(uni); // clone the current compatible bindings 
+                        for (newKey of newKeys) { // and add the new compatible bindings to the set of bindings
+                            nextUni[newKey] = binding[newKey];
+                        }
+                        iterativeBindings.push(nextUni); // then update iterativeBindings (which is reset to empty every unify)
+                    }
+                }
+            }
+            // console.log("iterartive", iterativeBindings);
+            possibleBindings = iterativeBindings;
+        } else {
+            continue;
+        }
+    }
+    return possibleBindings; 
 }
 
 /* current implementation compares entire objects, not individual sub-objects for example: 
@@ -102,38 +195,40 @@ function query(statement) {
  * rather all of the top rows in all the tables are being compared 
  * against all the bottom rows in all the tables
  */
-function equivQuery(pStatement) { // next step return list of key value pair sets like in unify
-    let unis = [];
-    unis.push(unify(pStatement[1]));
-    for (let i = 2; i < pStatement.length; i++) {
-        let newUni = unify(pStatement[i]);
-        console.log(newUni);
-        let newUniValues = [];
-        let prevUniValues = [];
-        for (let j = 0; j < newUni.length; j++) {
-            newUniValues.push(Object.values(newUni[j]));
-            prevUniValues.push(Object.values(unis[i-2][j]));
-        }
-        console.log(prevUniValues);
-        console.log(newUniValues);
-        if (newUniValues.toString() == prevUniValues.toString()) {
-            unis.push(newUni);
-        } else {
-            return false;
-        }
-    }
-    console.log(unis);
-    return true;
-}
+// function equivQuery(pStatement) { // next step return list of key value pair sets like in unify
+//     let unis = [];
+//     unis.push(unify(pStatement[1]));
+//     for (let i = 2; i < pStatement.length; i++) {
+//         let newUni = unify(pStatement[i]);
+//         console.log(newUni);
+//         let newUniValues = [];
+//         let prevUniValues = [];
+//         for (let j = 0; j < newUni.length; j++) {
+//             newUniValues.push(Object.values(newUni[j]));
+//             prevUniValues.push(Object.values(unis[i-2][j]));
+//         }
+//         console.log(prevUniValues);
+//         console.log(newUniValues);
+//         if (newUniValues.toString() == prevUniValues.toString()) {
+//             unis.push(newUni);
+//         } else {
+//             return false;
+//         }
+//     }
+//     console.log(unis);
+//     return true;
+// }
 
 function unifyAll(sentenceSet) { // next step return list of key value pair sets like in unify
+    console.log("Unifying Sentences: ", sentenceSet);
     let possibleBindings = [ {} ];
 
     for (let i = 0; i < sentenceSet.length; i++) {
         let iterativeBindings = [];
         let newBinding = unify(sentenceSet[i]);
-        // console.log("New Binding", newBinding);
+        //console.log("New Binding", newBinding);
         for (let uni of possibleBindings) {
+            //console.log("Uni: ", uni);
             for (let binding of newBinding) {
                 let newKeys = Object.keys(binding).filter(k=>!Object.keys(uni).includes(k)); // returns all the keys that are not in uni
                 let oldKeys = Object.keys(binding).filter(k=>Object.keys(uni).includes(k)); //  returns all the keys that are in uni
@@ -155,7 +250,7 @@ function unifyAll(sentenceSet) { // next step return list of key value pair sets
     return possibleBindings;
 }
 
-module.exports = { parseSentence, insert, remove, unify, parseStatement, query, unifyAll, DB } 
+module.exports = { parseSentence, insert, remove, unify, query, unifyAll, DB } 
 
 // 1st loop [ { Y: 'woof' }, { Y: 'meow' } ]
 
--- practiceParser.js
+++ practiceParser.js
@@ -39,6 +39,35 @@ sentences.push("fizz.buzz.foo");
 sentences.push("some.other.woof");
 sentences.push("some.other.meow");
 
+sentences.push("practice.ticTacToe.joe.bob.board.top.l!x");
+sentences.push("practice.ticTacToe.joe.bob.board.middle.l!x");
+sentences.push("practice.ticTacToe.joe.bob.board.bottom.l!x");
+sentences.push("practice.ticTacToe.joe.bob.board.top.c!o");
+sentences.push("practice.ticTacToe.joe.bob.board.middle.c!o");
+sentences.push("practice.ticTacToe.joe.bob.board.bottom.c!empty");
+sentences.push("practice.ticTacToe.joe.bob.board.top.r!x");
+sentences.push("practice.ticTacToe.joe.bob.board.middle.r!empty");
+sentences.push("practice.ticTacToe.joe.bob.board.bottom.r!o");
+sentences.push("practice.ticTacToe.joe.bob.player.joe.piece!x");
+sentences.push("practice.ticTacToe.joe.bob.player.bob.piece!o");
+
+sentences.push("practice.ticTacToe.joe.bill.board.top.l!x");
+sentences.push("practice.ticTacToe.joe.bill.board.middle.l!x");
+sentences.push("practice.ticTacToe.joe.bill.board.bottom.l!x");
+sentences.push("practice.ticTacToe.joe.bill.board.top.c!o");
+sentences.push("practice.ticTacToe.joe.bill.board.middle.c!o");
+sentences.push("practice.ticTacToe.joe.bill.board.bottom.c!empty");
+sentences.push("practice.ticTacToe.joe.bill.board.top.r!x");
+sentences.push("practice.ticTacToe.joe.bill.board.middle.r!empty");
+sentences.push("practice.ticTacToe.joe.bill.board.bottom.r!o");
+sentences.push("practice.ticTacToe.joe.bill.player.joe.piece!x");
+sentences.push("practice.ticTacToe.joe.bill.player.bill.piece!o");
+
+/* x o x
+ * x o e
+ * x e o
+*/
+
 for (s of sentences) {
     tokenList.push(DB.parseSentence(s)); 
     DB.insert(s);
@@ -49,8 +78,16 @@ DB.remove("practice.ticTacToe.table1.whoseMove");
 // console.log(DB.unify("practice.ticTacToe.T.players.P"));
 // console.log(DB.query("EQ practice.ticTacToe.T.board.top.l.P1 practice.ticTacToe.T.board.bottom.l.P2")); // next step
 // console.log(DB.query("EQ practice.ticTacToe.table1.board.top.l.P1 practice.ticTacToe.table1.board.center.l.P2 practice.ticTacToe.table1.board.bottom.l.P3"));
-// console.log(DB.query("EQ practice.ticTacToe.T.players.P1 practice.ticTacToe.T.players.P2"));
-console.log(DB.unifyAll(["fizz.buzz.X" ,"some.other.Y",  "X.Y.woof" ]))
+console.log(DB.query(["practice.ticTacToe.Player1.Player2.board.top.Col!Piece",
+                      "practice.ticTacToe.Player1.Player2.board.middle.Col!Piece",
+                      "practice.ticTacToe.Player1.Player2.board.bottom.Col!Piece",
+                      "practice.ticTacToe.Player1.Player2.player.Winner.piece!Piece",
+                      // Grab the loser so we can mark them as having lost
+                      "practice.ticTacToe.Player1.Player2.player.Loser",
+                      "NOT practice.ticTacToe.Player1.Player2.player.bill",
+                      "NEQ Winner Loser"]));
+
+// console.log(DB.unifyAll(["fizz.buzz.X" ,"some.other.Y",  "X.Y.woof" ]))
 // console.log(JSON.stringify(DB.DB,null,4));
 
 //console.log(tokenList);
