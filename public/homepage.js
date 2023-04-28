c8d59ed7d7cfb552700043ce60c62ffa014f6bea
Rosaura Hernandez Roman <40476532+rosieeeh@users.noreply.github.com>
2023-04-28 15:27:07 -0700

    Added popups for added char and actions

--- public/homepage.js
+++ public/homepage.js
@@ -20,6 +20,13 @@ function addChar(){
         console.log(window.DB);
         console.log("added");
     }
+    //adding a pop up for when character created
+    var word = document.getElementById("pop-up");
+    if(word.innerHTML == " "){
+        word.innerHTML = newchar + ": Added to the Stage";
+    }
+    setTimeout(function() {document.getElementById('pop-up').innerHTML=" ";},2000);
+
     document.getElementById("newname").value = "";
 }
 function doneWCharac(){
@@ -50,7 +57,14 @@ function take_action(action){
     char_actions = document.getElementById(`${play_as} Actions`);
     var newAction = document.createElement('li')
     newAction.textContent = action.innerText;
-    
+
+     //Pop up for action taken
+    var action_taken = document.getElementById("pop-up");
+    if(action_taken.innerHTML == " "){
+        action_taken.innerHTML = play_as + ": " + action.innerText;
+    }
+    setTimeout(function() {document.getElementById('pop-up').innerHTML=" ";},2000);
+
     if (char_actions.childNodes[0].innerText == "No Actions Taken") {
         char_actions.removeChild(char_actions.childNodes[0]);
     }
@@ -75,6 +89,11 @@ function display(){ // change to be on each action (when character is added, say
         charList.appendChild(def);
         charItem.appendChild(charList);
         document.getElementById('character-list').appendChild(charItem); 
-    }        
+    }  
+    //var element = document.getElementById('sidebar');
+    //element.scrollTop = element.scrollHeight; 
+    var topDiv = document.getElementById('sidebar').offsetTop;
+    var heightDiv = document.getElementById('sidebar').offsetHeight;
+    window.scrollTo({top:(topDiv+heightDiv)});
 }
 
